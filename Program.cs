e188600b46f027cc3be5976fdfa22728e82f5ade
Will <doggy.huang@gmail.com>
2024-11-29 13:33:13 +0800

    初始化 EF Core 設定與連接字串

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using api1.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ContosoUniversityContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
